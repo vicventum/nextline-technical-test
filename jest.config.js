42d973bb63d1d115a4b09541abc9e347e19c713a
Víctor Álvarez <vicventum.job@gmail.com>
2024-04-06 15:42:45 -0400

    Configura Testing Library en el proyecto

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,19 @@
 module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: [
-    'js',
-    'vue',
-    'json'
-  ],
+  moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
   },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
 }
--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,9 +1,13 @@
-import { mount } from '@vue/test-utils'
+// import '@testing-library/jest-dom'
+// import { mount } from '@vue/test-utils'
+import { render, screen } from '@testing-library/vue'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
 describe('NuxtLogo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
-    expect(wrapper.vm).toBeTruthy()
+  test('is a Vue instance', async () => {
+    render(NuxtLogo)
+    screen.debug()
+    const logo = await screen.findByRole('logo')
+    expect(logo).toBeInTheDocument()
   })
 })
