ce04f024b3b8a939ec5a60301f2bbb2b17b6f7b9
Víctor Álvarez <vicventum.job@gmail.com>
2024-04-06 18:46:45 -0400

    Configura Vuetify en los tests

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,13 +1,32 @@
-// import '@testing-library/jest-dom'
-// import { mount } from '@vue/test-utils'
 import { render, screen } from '@testing-library/vue'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
 describe('NuxtLogo', () => {
+
   test('is a Vue instance', async () => {
     render(NuxtLogo)
     screen.debug()
-    const logo = await screen.findByRole('logo')
-    expect(logo).toBeInTheDocument()
+    const button = await screen.findByRole('button')
+    expect(button).toBeInTheDocument()
   })
 })
+
+// import { createLocalVue, mount } from '@vue/test-utils'
+// import Vuetify from 'vuetify'
+// import NuxtLogo from '@/components/NuxtLogo.vue'
+
+// describe('Index Switch', () => {
+//   const localVue = createLocalVue()
+//   let vuetify
+
+//   beforeEach(() => {
+//     vuetify = new Vuetify()
+//   })
+
+//   it('should have index switch button', () => {
+//     const wrapper = mount(NuxtLogo, { localVue, vuetify })
+//     console.log("🚀 ~ it ~ wrapper:", wrapper.html())
+//     const myButton = wrapper.find('.v-btn')
+//     expect(myButton.text()).toBe('text')
+//   })
+// })
