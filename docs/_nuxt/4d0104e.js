63e170721bba377b29c7554173ecc42dadca3ad5
Víctor Álvarez <vicventum.job@gmail.com>
2024-04-08 22:55:28 -0400

    Actualiza cambios para GitHub Pages

--- docs/_nuxt/4d0104e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{605:function(e,n,t){"use strict";t.r(n);var c={components:{TaskDetail:t(598).default}},l=t(64),component=Object(l.a)(c,(function(){var e=this._self._c;return e("div",[e("TaskDetail")],1)}),[],!1,null,"306ec9c7",null);n.default=component.exports}}]);
